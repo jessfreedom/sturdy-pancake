192a810b092a420c1401ecfe276fdce3634ac37c
[Jess] <[jesscyz97@outlook.com]>
2020-02-24 21:19:17 +1300

    outputting dynamic content & working with props

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h2>Hi, I'm a React App</h2>
+        <p> This is really working!</p>
+        <button>Switch Name</button>
+        <Person name  = "Max" age = "28"/> 
+        <Person name = "Jess" age = "22"> My Hobbies: Racing </Person>
+        <Person name = "Manu" age = "29"/> 
+
       </div>
     );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m an React app'))
   }
 }
 
