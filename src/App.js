a85ee3bb5db8ccce33013b1a13acebb7c126964b
[Jess] <[jesscyz97@outlook.com]>
2020-02-25 17:16:33 +1300

    adding two way binding
    dynamically call an event (passing through method references) and handling inputs

--- src/App.js
+++ src/App.js
@@ -22,9 +22,20 @@ class App extends Component {
       {name: 'Jess', age: '22'},
       {name: 'Manu', age: '10000'}
     ]
-    }
-      )
+    })
   }
+
+  nameChangedHandler = (event) => {
+    this.setState(
+      {
+        persons:[
+          {name: 'Max' , age: '28'},
+          {name: event.target.value, age: '22'},
+          {name: 'Manu', age:'10000'}
+        ]
+      })
+  }
+
 
   render() {
 
@@ -40,7 +51,8 @@ class App extends Component {
         <Person 
           name={this.state.persons[1].name} 
           age={this.state.persons[1].age}
-          click = {this.switchNameHandler.bind(this, 'Max!!')}> My Hobbies: Racing </Person>
+          click = {this.switchNameHandler.bind(this, 'Max!!')}
+          changed = {this.nameChangedHandler}> My Hobbies: Racing </Person>
         <Person 
           name={this.state.persons[2].name} 
           age={this.state.persons[2].age}/> 
